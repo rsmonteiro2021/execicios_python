814d6c62ac49c3e9faf9503454074b8e9b94210d
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-04-29 18:35:27 -0300

    Update car.py

--- cap_9/listagens/car.py
+++ cap_9/listagens/car.py
@@ -19,10 +19,15 @@ class Car():
         return long_name.title()
     
     def read_odometer(self):
-        """Exibe uma frase que mostra amilhagem do carro."""
+        """Exibe uma frase que mostra a milhagem do carro """
         print("This car has " + str(self.odometer_reading) + " miles on it.")
+    
+    def update_odometer(self, mileage):
+        """Define o valor de leitura do hodômetro com valor especificado."""
+        self.odometer_reading = mileage
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
+
+my_new_car.update_odometer(23)
 my_new_car.read_odometer()
