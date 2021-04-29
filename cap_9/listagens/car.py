1a92a20109980dd802b612ebeaf08ff96e7f8def
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-04-29 18:04:30 -0300

    Update car.py

--- cap_9/listagens/car.py
+++ cap_9/listagens/car.py
@@ -11,11 +11,18 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
         
     def get_descriptive_name(self):
         """Devolve um nome descritivo, formatado de modo elegante ."""
         long_name = str(self.year) + " " + self.make + " " + self.model
         return long_name.title()
+    
+    def read_odometer(self):
+        """Exibe uma frase que mostra amilhagem do carro."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 
+my_new_car.read_odometer()
