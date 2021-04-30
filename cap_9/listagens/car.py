331cab33e67557c95735baa347dae6650fa97c02
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-04-29 23:21:27 -0300

    Update car.py

--- cap_9/listagens/car.py
+++ cap_9/listagens/car.py
@@ -24,5 +24,6 @@ class Car():
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
+my_new_car.odometer_reading = 23 # uma forma de alterar o valor de um atributo diretamente
 my_new_car.read_odometer()
+
