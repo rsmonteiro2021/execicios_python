85f4e5c3eb7fa3e5fb56e8f2419b6030583d9bed
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-04-30 23:58:07 -0300

    Create 9.5.py

---
+++ cap_9/exercicios/9.5.py
@@ -0,0 +1,61 @@
+"""
+    Tentativas de Login: Acrescente um atibuto chamado login_attempts à sua classe User
+    do Exercício 9.3 (página 226). Escreva um método chamado increment_login_attempts()
+    que incremente o valor de login_attempts em 1. Escreva outro método chamado
+    reset_login_attempts() que reinicie o valor de login_attempts com O.
+"""
+"""
+    Crie uma instância da classe User e chame increment_login_attempts() várias vezes.
+    Exiba o valor de login_attempts para garantir que ele foi incrementado de forma apropriada
+    e, em seguida, chame reset_login_attempts(). Exiba login_attempts novamente para garantir
+    que seu valor foi reiniciado com O.
+"""
+# Relembrando o exercício 9.3
+
+"""
+    Usuários: Crie uma classe chamada User. Crie dois atributos de nomes first_name e last_name e,
+    então, crie vários outros atributos normalmente armazenados em um perfil de usuário. Escreva
+    um método de nome describe_user() que apresente um resumo das informações do usuário. Escreva
+    Escreva outro método chamado greet_user() que mostre uma saudação personalizada ao usuário.
+    Crie várias instâncias que representem diferentes usuários e chame os dois métodos para cada
+    usuário.
+"""
+
+class User():
+    """Tentativa simples de descrever um usuário."""
+    def __init__(self, first_name, last_name, login_attempts):
+        """Inicia os atributos do usuário."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login_attempts = login_attempts
+
+    def describe_user(self):
+        """Exibe os atributos do usuário."""
+        description = {'First name': self.first_name.title(), 'Last name': self.last_name.title(),'Login_Attempts': str(self.login_attempts)}
+        for value, key in description.items():
+            print(value + ': ' + key)
+    
+    def greet_user(self):
+        """Retorna uma saudação personalizada"""
+        saudacao = '\tOlá, Sr(a) ' + self.first_name.title() + ' ' + self.last_name.title() + '!\n' + '\tSeja bem vindo(a)!'
+        return saudacao
+
+    def increment_login_attempts(self, login):
+        self.login_attempts += login
+
+    def reset_login_attempts(self, login):
+        self.login_attempts = login
+
+usuario = User('Roberto', 'Monteiro', 1)
+print('Informações do Usuário:')
+usuario.describe_user()
+print('Menagem de boas vindas:')
+print(usuario.greet_user())
+usuario.increment_login_attempts(1)
+print('Informações do Usuário:')
+usuario.describe_user()
+usuario.increment_login_attempts(1)
+print('Informações do Usuário:')
+usuario.describe_user()
+usuario.reset_login_attempts(0)
+usuario.describe_user()
