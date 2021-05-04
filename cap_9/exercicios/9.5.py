c8340f2ea2f2975c03dea393de615bfed08f634d
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-05-04 20:36:07 -0300

    Update 9.5.py

--- cap_9/exercicios/9.5.py
+++ cap_9/exercicios/9.5.py
@@ -46,16 +46,48 @@ class User():
     def reset_login_attempts(self, login):
         self.login_attempts = login
 
-usuario = User('Roberto', 'Monteiro', 1)
-print('Informações do Usuário:')
-usuario.describe_user()
-print('Menagem de boas vindas:')
-print(usuario.greet_user())
-usuario.increment_login_attempts(1)
-print('Informações do Usuário:')
-usuario.describe_user()
-usuario.increment_login_attempts(1)
-print('Informações do Usuário:')
-usuario.describe_user()
-usuario.reset_login_attempts(0)
-usuario.describe_user()
+first_name = input('Digite o primeiro nome do usuário:\n')
+last_name = input('Digite o último nome do usuário:\n')
+full_name = first_name + " " + last_name
+usuarios = ['Roberto Monteiro', 'Kesia Jacquline']
+usuario = User(first_name, last_name, 1)
+if full_name in usuarios:
+    print('Informações do Usuário:')
+    usuario.describe_user()
+    print('Menagem de boas vindas:')
+    print(usuario.greet_user())
+    usuario.increment_login_attempts(1)
+else:
+    print('Usuário não cadastrado. Você tem mais 3 tentaiva!')
+    while usuario.login_attempts > 0 and usuario.login_attempts <= 3:
+        first_name = input('Digite o primeiro nome do usuário:\n')
+        last_name = input('Digite o último nome do usuário:\n')
+        full_name = first_name + " " + last_name
+        usuario = User(first_name, last_name, usuario.login_attempts)
+        if usuario.login_attempts > 0 and usuario.login_attempts < 3:
+            if full_name in usuarios:
+                print('Informações do Usuário:')
+                usuario.describe_user()
+                print('Menagem de boas vindas:')
+                print(usuario.greet_user())
+                usuario.increment_login_attempts(1)
+                break
+            else:
+                print('Informações do Usuário:')
+                usuario.describe_user()
+                print(full_name + ' não está cadastrado.')
+                usuario.increment_login_attempts(1)
+        else:
+            if full_name in usuarios:
+                print('Informações do Usuário:')
+                usuario.describe_user()
+                print('Menagem de boas vindas:')
+                print(usuario.greet_user())
+                usuario.increment_login_attempts(1)
+                break
+            else:
+                print('Informações do Usuário:')
+                usuario.describe_user()
+                print(full_name + ' não está cadastrado.')
+                usuario.reset_login_attempts(0)
+                print('Você deve esperar alguns minutos para tentar logar novamente!')
