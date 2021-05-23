e28f6b2d771f6b9ea5d6c18df63077d0ba418cf3
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-05-23 19:03:40 -0300

    Update 9.9.py

--- cap_9/exercicios/9.9.py
+++ cap_9/exercicios/9.9.py
@@ -2,7 +2,7 @@
     Upgrade de bateria: User a última versão de Electric.car.py desta seção. Acrescente
     um método chamado upgrade_battery() na classe Battery. Esse método deve verificar
     a capacidade da bateria e defini-la como 85 se o valor for diferente. Crie um carro
-    elétrico com uma capacidade de bateria default, chame get_range() uma vez e e, seguida
+    elétrico com uma capacidade de bateria default, chame get_range() uma vez e em, seguida
     chame get_ranger() uma segunda vez após fazer um upgrade da bateria. Você deverá ver
     um aumento na distância que o carro é capaz de percorrer.
 """
@@ -71,7 +71,68 @@ class EletricCar(Car):
         específicos de um carro elétrico."""
 
         super().__init__(make, model, year):
-        self_battery = Battery()
+        self.battery = Battery()
+        self.details_car = 'details_car'
+    
+    def describe_battery(self):
+        """Exibe uma frase que descreve a capacidade da bateria."""
+        print("This car has a " + str(self.battery_size) + " -kwh battery.")
+
+    def describe_details(self):
+        """Retorna detalhes do carro elétrico."""
+
+        while True:
+            disponible_color = {1: 'White', 2: 'Black', 3: 'Silver', 4: 'Red'}
+            print('Escolha a cor do seu Tesla Eletric Car. \nDisponible Color:')
+            for number, color in disponible_color.items():
+                print('\t' + str(number) + ': ' + color + '.')
+            color = int(input('Digite aqui sua cor favorita:\n'))
+
+            if color in disponible_color:
+            
+                if color == 1:
+                    your_car = {'Color': 'White', 'Horse Power': '80 hp', 'Torque': '14 kNm', 'Style': 'Sedan'}
+                    print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
+                    for value, key in your_car.items():
+                        print('\t' + value + ': ' + key)
+                    break
+                elif color == 2:
+                    your_car = {'Color': 'Black', 'Horse Power': '80 hp', 'Torque': '14 kNm', 'Style': 'Hatch'}
+                    print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
+                    for value, key in your_car.items():
+                        print('\t' + value + ': ' + key)
+                    break
+                elif color == 3:
+                    your_car = {'Color': 'Silver', 'Horse Power': '90 hp', 'Torque': '16 kNm', 'Style': 'SUV'}
+                    print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
+                    for value, key in your_car.items():
+                        print('\t' + value + ': ' + key)
+                    break
+                else:
+                    your_car = {'Color': 'Red', 'Power': '116 hp', 'Torque': '18 kNm', 'Style': 'Cupê'}
+                    print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
+                    for value, key in your_car.items():
+                        print('\t' + value + ': ' + key)
+                    break
+            else:
+                print('Não estamos personalizando o carro elétrico no momento!')
+
+"""my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_new_car.increment_odometer(100)
+my_new_car.read_odometer()
+"""
+my_tesla = EletricCar("tesla", "model's", 2022)
+my_tesla.describe_details()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+print('\t' + my_tesla.get_descriptive_name())
+my_tesla.battery.get_range()
+print('\t' + my_tesla.get_descriptive_name())
 
 
 
