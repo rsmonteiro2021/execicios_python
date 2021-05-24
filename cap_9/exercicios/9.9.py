13f1b72699b218f52c6767eeef71580ba493f59f
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-05-23 23:58:23 -0300

    Update 9.9.py

--- cap_9/exercicios/9.9.py
+++ cap_9/exercicios/9.9.py
@@ -6,7 +6,7 @@
     chame get_ranger() uma segunda vez após fazer um upgrade da bateria. Você deverá ver
     um aumento na distância que o carro é capaz de percorrer.
 """
-class car():
+class Car():
     """Tentativa simples de modelar um carro."""
 
     def __init__(self, make, model, year):
@@ -16,7 +16,7 @@ class car():
         self.year = year
         self.odometer_reading = 0
 
-    def get_descriptive_car(self):
+    def get_descriptive_name(self):
         """ Retonra o nome do veículo de modo formatado."""
         long_name = str(self.year) + ', ' + self.make.title() + ', ' + self.model.title()
         return long_name
@@ -50,14 +50,13 @@ class Battery():
         message = 'This car has a ' + str(self.battery_size) + ' kwh battery.'
         return message
 
-    def get_ranger(self):
+    def get_range(self):
         """Exibe uma frase acerca da distância que o carro é capaz de percorrer com essa bateria."""
         if self.battery_size == 70:
             range = 240
         elif self.battery_size == 85:
             range = 270
-        message = 'This car has a ' + str(self.battery_size) + ' kwh battery.'
-        return message
+        print = 'This car has a ' + str(self.battery_size) + ' kwh battery.'
 
     def upgrade_battery(self, battery_size = 85):
         """Exibe a capacidade da bateria."""
@@ -70,7 +69,7 @@ class EletricCar(Car):
         """Inicializa os atributos da classe pai. Em seguida, inicializa os atributos
         específicos de um carro elétrico."""
 
-        super().__init__(make, model, year):
+        super().__init__(make, model, year)
         self.battery = Battery()
         self.details_car = 'details_car'
     
@@ -91,25 +90,25 @@ class EletricCar(Car):
             if color in disponible_color:
             
                 if color == 1:
-                    your_car = {'Color': 'White', 'Horse Power': '80 hp', 'Torque': '14 kNm', 'Style': 'Sedan'}
+                    your_car = {'Color': 'White', 'Horse Power': '126 hp', 'Torque': '14 kNm', 'Style': 'Sedan'}
                     print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
                     for value, key in your_car.items():
                         print('\t' + value + ': ' + key)
                     break
                 elif color == 2:
-                    your_car = {'Color': 'Black', 'Horse Power': '80 hp', 'Torque': '14 kNm', 'Style': 'Hatch'}
+                    your_car = {'Color': 'Black', 'Horse Power': '136 hp', 'Torque': '14 kNm', 'Style': 'Hatch'}
                     print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
                     for value, key in your_car.items():
                         print('\t' + value + ': ' + key)
                     break
                 elif color == 3:
-                    your_car = {'Color': 'Silver', 'Horse Power': '90 hp', 'Torque': '16 kNm', 'Style': 'SUV'}
+                    your_car = {'Color': 'Silver', 'Horse Power': '126 hp', 'Torque': '16 kNm', 'Style': 'SUV'}
                     print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
                     for value, key in your_car.items():
                         print('\t' + value + ': ' + key)
                     break
                 else:
-                    your_car = {'Color': 'Red', 'Power': '116 hp', 'Torque': '18 kNm', 'Style': 'Cupê'}
+                    your_car = {'Color': 'Red', 'Power': '150 hp', 'Torque': '18 kNm', 'Style': 'Cupê'}
                     print('Congratulations!!!!\nDetails of the your Tesla Electric Car:')
                     for value, key in your_car.items():
                         print('\t' + value + ': ' + key)
@@ -131,6 +130,7 @@ my_tesla.describe_details()
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
 print('\t' + my_tesla.get_descriptive_name())
+my_tesla.battery.upgrade_battery()
 my_tesla.battery.get_range()
 print('\t' + my_tesla.get_descriptive_name())
 
