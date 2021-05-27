89ac94f0f500315bbbe454f57daf384cf878300e
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-05-27 18:23:10 -0300

    Update my_cars.py

--- cap_9/listagens/my_cars.py
+++ cap_9/listagens/my_cars.py
@@ -1,7 +1,7 @@
-from car import Car, EletricCar
+import car
 
-my_beetle = Car('Volkswagem', 'beetle', 2016)
+my_beetle = car.Car('Volkswagem', 'beetle', 2016)
 print('\t-' + my_beetle.get_descriptive_name())
 
-my_tesla = EletricCar('tesla', 'roadster', 2016)
+my_tesla = car.EletricCar('tesla', 'roadster', 2016)
 print('\t-' + my_tesla.get_descriptive_name())
