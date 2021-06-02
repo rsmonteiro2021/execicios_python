fd5d6a45cd114675fcf99340c5d3f1fccb7ee046
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-06-01 21:50:52 -0300

    update cap_10

--- cap_10/listas/file_reader.py
+++ cap_10/listas/file_reader.py
@@ -1,3 +1,13 @@
+# Uma forma de ler um arquivo de texto em python:
+
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
-    print(contents)
+    print(contents + '\n')
+
+# Essa é outra forma de ler um arquivo de texto em python sendo linha a linha:
+
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip()) # foi inserido a função rstrip() para que o resultado fosse igual ao anterior.
