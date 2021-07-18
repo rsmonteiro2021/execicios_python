6563c6169a444806e1999a35433b13574b2a537f
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-07-18 18:35:01 -0300

    Update alice.py

--- cap_10/listas/alice.py
+++ cap_10/listas/alice.py
@@ -2,8 +2,8 @@
 """
 filename = 'alice.txt'
 try:
-    with open(filename) as file_object:
-        contents = file_object.read()
+    with open(filename) as f_obj:
+        contents = f_obj.read()
 except FileNotFoundError:
     msg = "Sorry, the file " + filename + " does not exist."
     print(msg)
