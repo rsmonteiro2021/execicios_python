41c515c9ab70b9086183cc499314d9a2b0f6a8b7
rsmonteiro2021 <monteiroengenharia@gmail.com>
2021-07-18 18:54:40 -0300

    update
    foi acrescido o encoding 'utf-8'

---
+++ cap_10/listas/word_count.py
@@ -0,0 +1,17 @@
+def count_words(filename):
+    """Conta o número aproximado de palavras em um arquivo."""
+    
+    try:
+        with open(filename) as f_obj:
+            contents = f_obj.read()
+    except FileNameFoundError:
+        msg = "Sorry, the file " + filename + " does not exist."
+        print(msg)
+    else:
+        #Conta o número aproximado de palavras no arquivo.
+        words = contents.split()
+        num_words = len(words)
+        print("The file " + " has about " + str(num_words) + " words.")
+        
+    filename = 'alice.txt'
+    count_words(filename)
